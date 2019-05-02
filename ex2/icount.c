39287f3cf8bae2589828e715c4705d4df459e547
Luiz <luizhreis.gris@gmail.com>
2019-05-02 00:18:38 -0300

    quase lá

--- ex2/icount.c
+++ ex2/icount.c
@@ -5,8 +5,6 @@
 #include <sys/types.h>
 #include <errno.h>
 
-enum Type {S_IFDIR = 'S_IFDIR - diretorio', S_IFLNK = 'S_IFLNK - link simbolico', S_IFBLK = 'S_IFBLK - arquivo de bloco', S_IFCHR = 'S_IFCHR - arquivo de caracter', S_IFREG = 'S_IFREG - arquivo normal'};
-
 int counter = 0;
 int type_counted = S_IFREG;
 int recursive_mode = 0;
@@ -41,24 +39,29 @@ void count_files(char *path){
 int main(int argc, char **argv){
 	int opt;
 	int opterr = 0;
+	int total = 0;
+	char *type_string = "S_IFDIR (arquivo normal)";
 	while((opt = getopt(argc,argv,"Rrdlbc")) != -1){
 		switch(opt){
 			case 'd':
 				type_counted = S_IFDIR;
+				type_string = "S_IFDIR (diretorio)";
 				break;
 			case 'l':
 				type_counted = S_IFLNK;
+				type_string = "S_IFLNK (link simbolico)";
 				break;	
 			case 'b':
 				type_counted = S_IFBLK;
+				type_string = "S_IFBLK (arquivo de bloco)";
 				break;
 			case 'c':
 				type_counted = S_IFCHR;
+				type_string = "S_IFCHR (arquivo de caracter)";
 				break;
 			case 'R':
 				recursive_mode = 1;
 				break;
-			case '?':
 			case 'r':
 			default:
 				type_counted = S_IFREG;
@@ -66,14 +69,16 @@ int main(int argc, char **argv){
 
 		}
 	}
-	printf("type_counted: %d,  S_IFREG: %d\n",type_counted,S_IFREG);
+	// printf("type_counted: %d,  S_IFREG: %d\n",type_counted,S_IFREG);
     printf("# args = %d\n", optind);
 	for(; optind < argc; optind++){
 		printf("WALKING THROUGH DIR %s\n", argv[optind]);
 		walk_dir(argv[optind], count_files);
-		printf("TOTAL: %d arquivos do tipo pedido.\n",counter);
+		printf("TOTAL: %d files of type %s on path '%s'.\n",counter, type_string, argv[optind]);
+		total += counter;
 		counter = 0;
 	}
+	printf("counted %d files of type %s.\n", total, type_string);
 
 	return 0;
 }
